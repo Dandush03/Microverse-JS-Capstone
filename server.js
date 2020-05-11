2cc4244c106738d0da440daab78498db399dbc68
Daniel Laloush <d.laloush@outlook.com>
2020-05-11 10:41:20 -0400

    fixing Linters Issues

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
 const server = app.listen(app.get('port'), () => {
+  // eslint-disable-next-line no-console
   console.log('listening on port ', server.address().port);
 });
